eff35f7e6f39f1c60fc8c2bfff5c974b6a8014af
Ankit <ankitrathaur.du.or.26@gmail.com>
2026-07-18 08:51:13 +0530

    Add data

---
+++ Day 70 - K-means clustering/data.py
@@ -0,0 +1,12 @@
+# Import Dataset and Libraries
+from sklearn.datasets import make_blobs 
+import matplotlib.pyplot as plt 
+
+# Others
+import pandas as pd
+import numpy as np
+
+# centoids
+centroids = [(-5,-5),(5,5)]
+cluster_std = [1,1] 
+
